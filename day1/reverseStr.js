86a7eaef7b67229b9d174c7140434552e376c57a
Mirko <ruhlmirkojoel@gmail.com>
2025-03-30 11:28:51 -0300

    Added two codewars exercises

---
+++ day1/reverseStr.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const rs = function (str) {
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+};
+
+console.log(rs("world"));
