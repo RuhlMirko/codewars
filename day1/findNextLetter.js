295334007612b8e8e0cd3e222f6ba7d1b276c8a6
Mirko <ruhlmirkojoel@gmail.com>
2025-04-06 18:54:06 -0300

    Added find letter kata

---
+++ day1/findNextLetter.js
@@ -0,0 +1,15 @@
+"use strict";
+function findMissingLetter(array) {
+  let codes = array.map((e) => e.charCodeAt(0));
+  console.log(codes);
+
+  for (let i in codes) {
+    if (codes[i] + 1 != codes[i + 1]) {
+      return String.fromCharCode(codes[i] + 1);
+    }
+  }
+  return " ";
+}
+
+console.log(findMissingLetter(["a", "b", "c", "d", "e", "f"])); // e
+console.log(findMissingLetter(["O", "Q", "R", "S"])); // P
