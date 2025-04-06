0d8d77820c746e005b5f8d3e49cb83f71fd3f4d3
Mirko <ruhlmirkojoel@gmail.com>
2025-04-05 22:22:28 -0300

    Initial commit

---
+++ day1/calcFunc.js
@@ -0,0 +1,21 @@
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
+
+console.log(seven(times(five()))); // should return 35
+console.log(seven(plus(five()))); // should return 12
+console.log(four(plus(nine()))); // should return 13
+console.log(eight(minus(three()))); // should return 5
+console.log(six(dividedBy(two()))); // should return 3
