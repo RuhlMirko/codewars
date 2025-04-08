34ec8147adf1f2d8406beaa143564d75dd7d2647
Mirko <ruhlmirkojoel@gmail.com>
2025-04-08 20:33:16 -0300

    Added an exercise that checks if a number is even or odd

---
+++ fundamentals/day3.4.js
@@ -0,0 +1,8 @@
+function evenOrOdd(number) {
+  return number % 2 == 0 ? "Even" : "Odd";
+}
+
+console.log(evenOrOdd(7)); // 'Odd'
+console.log(evenOrOdd(120)); // 'Even'
+console.log(evenOrOdd(23)); // 'Odd'
+console.log(evenOrOdd(-81)); // 'Odd'
