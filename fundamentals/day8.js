739d0fd51622307c0661e998fde21bf49b8b57b6
Mirko <ruhlmirkojoel@gmail.com>
2025-04-15 16:28:38 -0300

    Completed exercise

--- fundamentals/day8.js
+++ fundamentals/day8.js
@@ -2,10 +2,14 @@ function towerBuilder(nFloors) {
   const newStr = [];
   for (let i = 0; i < nFloors; i++) {
     let stars = i == 0 ? "*" : "*".repeat(2 * i + 1); // Number of stars for the current row
-    let spaces = " ".repeat(nFloors - i - 1); // Number of spaces before the stars to center them
-    newStr.push(spaces + stars);
+    let spaces =
+      i == 0 ? " ".repeat(nFloors - i - 1) : " ".repeat(nFloors - i - 1); // Number of spaces before the stars to center them
+    newStr.push(spaces + stars + spaces);
   }
-  return newStr.join("\n");
+  return newStr;
+  /*One-liner
+  return [...Array(n)].map((_,i)=>" ".repeat(n-1-i)+"*".repeat(i*2+1)+" ".repeat(n-1-i))
+*/
 }
 
 console.log(towerBuilder(1));
