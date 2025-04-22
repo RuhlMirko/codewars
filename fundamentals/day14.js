eb00bffdf4d5d64c2daa96694ce1094144745d4c
Mirko <ruhlmirkojoel@gmail.com>
2025-04-22 11:16:19 -0300

    Added exercise of the day

--- fundamentals/day14.js
+++ fundamentals/day14.js
@@ -1,9 +1,36 @@
 function twoSum(numbers, target) {
-  return [0, 1];
+  let match = [];
+  for (let i in numbers) {
+    for (let j in numbers) {
+      if (numbers[j] + numbers[i] == target) {
+        if (i != j) {
+          match = [i, j];
+        }
+      }
+    }
+  }
+  return match;
 }
+
+/*
+function twoSum(numbers, target) {
+  const seen = new Map();
+
+  for (let i = 0; i < numbers.length; i++) {
+    const complement = target - numbers[i];
+    if (seen.has(complement)) {
+      return [seen.get(complement), i];
+    }
+    seen.set(numbers[i], i);
+  }
+}
+
+*/
 
 console.log(twoSum([1, 2, 3], 4)); // returns [0,2] or [2,0]
 console.log(twoSum([3, 2, 4], 6)); // returns [1,2] or [2,1]
+console.log(twoSum([1234, 5678, 9012], 14690)); // 2 1
+console.log(twoSum([2, 2, 3], 4)); // 1 0
 
 /*
 Write a function that takes an array of numbers (integers for the tests) and a target number. 
