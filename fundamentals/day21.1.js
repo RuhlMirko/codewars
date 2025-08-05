187d73cda49915585ae52d63093dd188c5b04e03
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-04 21:52:20 -0300

    Added new function
    Added a shorter version of the function above

--- fundamentals/day21.1.js
+++ fundamentals/day21.1.js
@@ -25,6 +25,14 @@ function limitOccurrences(arr, n) {
   return result;
 }
 
+function sortDeleteNth(arr, x) {
+  var cache = {};
+  return arr.filter(function (n) {
+    cache[n] = (cache[n] || 0) + 1;
+    return cache[n] <= x;
+  });
+}
+
 console.log(deleteNth([20, 37, 20, 21], 1)); // 20,37,21
 console.log(limitOccurrences([20, 37, 20, 21], 1)); // 20,37,21
 console.log(deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3)); // 1, 1, 3, 3, 7, 2, 2, 2
