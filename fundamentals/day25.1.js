18348d6181ba812cef5568c19eca1d89bb59fb23
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-08 16:25:58 -0300

    Added new kata
    Added new exercise that simulates a traffic light state

---
+++ fundamentals/day25.1.js
@@ -0,0 +1,11 @@
+function updateLight(current) {
+  
+  return current == "green"? "yellow":current=="yellow"?"red":"green"
+
+}
+
+
+
+console.log(updateLight("green")) // yellow
+console.log(updateLight("yellow")) // red
+console.log(updateLight("red")) // green
