1c6d52795128e62694ff457c0e21a96bc71dfdfd
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-11 13:06:49 -0300

    Added new kata
    Added new exercise that returns an array without nearby repeated values

---
+++ fundamentals/day26.js
@@ -0,0 +1,14 @@
+var uniqueInOrder = function (iterable) {
+  const uniqueItems = [];
+  if (typeof iterable == "string") iterable = iterable.split("");
+  for (let i in iterable) {
+    i = Number(i);
+    if (iterable[i] !== iterable[i + 1]) uniqueItems.push(iterable[i]);
+  }
+
+  return uniqueItems;
+};
+
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+console.log(uniqueInOrder("ABBCcAD"));
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
