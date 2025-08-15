1d5c927aafba5611d838767f32d71cabb09fa3e3
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-15 12:02:54 -0300

    Added new kata
    This exercise checks if factor is factor of base

---
+++ fundamentals/day29.4.js
@@ -0,0 +1,10 @@
+function checkForFactor(base, factor) {
+  return base % factor == 0 ? true : false;
+}
+
+console.log(checkForFactor(10, 2));
+console.log(checkForFactor(9, 2));
+console.log(checkForFactor(653, 7));
+console.log(checkForFactor(63, 7));
+console.log(checkForFactor(2450, 5));
+console.log(checkForFactor(2453, 5));
