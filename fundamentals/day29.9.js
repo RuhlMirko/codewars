4f81d7e0eb72d3013d9bdb6cb4c7460e54387bcf
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-15 13:04:43 -0300

    Added one liner version

--- fundamentals/day29.9.js
+++ fundamentals/day29.9.js
@@ -2,6 +2,8 @@ function makeUpperCase(str) {
   return str.toUpperCase();
 }
 
+//const makeUpperCase = str => str.toUpperCase();
+
 console.log(makeUpperCase(""));
 console.log(makeUpperCase("hello"));
 console.log(makeUpperCase("HELLO"));
