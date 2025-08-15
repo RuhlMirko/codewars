592e8bfb3bb6f8f9933a63cbc5a3a9c18fb1c347
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-15 11:39:00 -0300

    Added new kata
    This kata takes two string numbers and returns a string sum of the two items

---
+++ fundamentals/day29.2.js
@@ -0,0 +1,8 @@
+function sumStr(a, b) {
+  return String(Number(a) + Number(b));
+}
+
+console.log(sumStr("4", "5")); //9
+console.log(sumStr("34", "5")); //39
+console.log(sumStr("", "")); //0
+console.log(sumStr("-5", "3")); //-2
