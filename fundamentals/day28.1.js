678d81343bec4dce79eb6e7f154606d79a1aa92b
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-14 21:19:27 -0300

    Added a one liner version

--- fundamentals/day28.1.js
+++ fundamentals/day28.1.js
@@ -11,6 +11,12 @@ function sumDigPow(a, b) {
   return retArr;
 }
 
+// One liner
+const EUREKAS = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 89, 135, 175, 518, 598, 1306, 1676, 2427, 2646798,
+];
+const newSumDigPow = (a, b) => EUREKAS.filter((n) => a <= n && n <= b);
+
 console.log(sumDigPow(1, 10)); // 1...10
 console.log(sumDigPow(1, 100)); // 1...10,89
 console.log(sumDigPow(1, 150)); // 1...10,89,135
