6a629e61a43e698fb182d503cc278dc15e0f4796
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-14 21:49:19 -0300

    Added a shorter version
    Added a version of the function that uses the built-in sort method

--- fundamentals/day28.3.js
+++ fundamentals/day28.3.js
@@ -10,5 +10,15 @@ function order(words, wordsArr = words.split(" ")) {
   return returnStr.join(" ");
 }
 
+function newOrder(words) {
+  return words
+    .split(" ")
+    .sort((a, b) => a.match(/\d/) - b.match(/\d/))
+    .join(" ");
+}
+
+console.log(newOrder("is2 Thi1s T4est 3a"));
+console.log(newOrder("4of Fo1r pe6ople g3ood th5e the2"));
+
 console.log(order("is2 Thi1s T4est 3a"));
 console.log(order("4of Fo1r pe6ople g3ood th5e the2"));
