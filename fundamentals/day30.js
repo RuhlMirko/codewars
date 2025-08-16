d9f8d84e0718ac083656a83aa580bb00895ae7b7
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-16 11:18:36 -0300

    Added the exercise as comment

--- fundamentals/day30.js
+++ fundamentals/day30.js
@@ -12,3 +12,15 @@ function expandedForm(num) {
 console.log(expandedForm(12)); // 10 + 2
 console.log(expandedForm(45)); // 40 + 5
 console.log(expandedForm(70304)); // 70000 + 300 + 4
+
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+//https://www.mathsisfun.com/definitions/expanded-notation.html
+
+//    12 --> "10 + 2"
+//    45 --> "40 + 5"
+// 70304 --> "70000 + 300 + 4"
+// NOTE: All numbers will be whole numbers greater than 0.
+
+// If you liked this kata, check out part 2!!
+//https://www.codewars.com/kata/write-number-in-expanded-form-part-2
