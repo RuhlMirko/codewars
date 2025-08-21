bff24fd8a0c507d98d795ba6b59ace10f31abef0
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-21 02:40:02 -0300

    Added new kata
    This kata has two functions that acomplish the same thing, it takes a string and sum a number to any number that could exist at the end of said string

---
+++ fundamentals/day33.js
@@ -0,0 +1,26 @@
+function incrementString(strng) {
+  // return incrementedString
+  const found = strng.match(/\d+$/);
+  if (!found) return strng + 1;
+  const foundNum = found[0];
+  const foundIndex = found.index;
+  const newNumber = parseInt(foundNum) + 1;
+
+  return (
+    strng.slice(0, foundIndex) +
+    String(newNumber).padStart(foundNum.length, "0")
+  );
+}
+
+function improvedIncrementString(input) {
+  if (isNaN(parseInt(input[input.length - 1]))) return input + "1";
+  return input.replace(/(0*)([0-9]+$)/, function (match, p1, p2) {
+    var up = parseInt(p2) + 1;
+    return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+  });
+}
+
+console.log(incrementString("foo")); // foo1
+console.log(incrementString("foo23")); // foo1
+console.log(incrementString("foo0042")); // foo1
+console.log(incrementString("fooll0042")); // foo1
