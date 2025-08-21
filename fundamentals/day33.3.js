ba1c556c62944e429ed5c61a7101a8fa4f0daaf4
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-21 13:33:45 -0300

    Added todo item

--- fundamentals/day33.3.js
+++ fundamentals/day33.3.js
@@ -11,3 +11,4 @@ console.log(sortArray([5, 8, 6, 3, 4])); // 3 8 6 5 4
 console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])); // 1 8 3 6 5 4 7 2 9 0
 console.log(sortArray([7, 2, 1])); // 1 2 7
 // console.log(sortArray());
+// TODO: Finish the function
