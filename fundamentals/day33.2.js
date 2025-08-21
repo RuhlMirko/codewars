e7018ca4297723ccaf750d244bb2eb30b971dae9
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-08-21 13:15:30 -0300

    Added shorter version

--- fundamentals/day33.2.js
+++ fundamentals/day33.2.js
@@ -7,3 +7,10 @@ function digPow(n, p) {
 console.log(digPow(89, 1)); // 1
 console.log(digPow(92, 1)); // -1
 console.log(digPow(46288, 3)); // 51
+
+function newDigPow(n, p) {
+  var x = String(n)
+    .split("")
+    .reduce((s, d, i) => s + Math.pow(d, p + i), 0);
+  return x % n ? -1 : x / n;
+}
