c3dd98e25a81499e795fc8612ad1018c951c70c9
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-01 14:49:05 -0300

    Updated to switch statement

--- fundamentals/day.35.js
+++ fundamentals/day.35.js
@@ -1,8 +1,18 @@
 function likes(names) {
-  let result = "";
-  const isPlural = names.length > 1 ? "like" : "likes";
-  if (!names.length) return `no one ${isPlural} this`;
-  return result;
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return names[0] + " likes this";
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${
+        names.length - 2
+      } others like this`;
+  }
 }
 
 console.log(likes([])); //"no one likes this"
@@ -10,3 +20,4 @@ console.log(likes(["Peter"])); //"Peter likes this"
 console.log(likes(["Peter", "Jacob"])); //"Jacob and Alex like this"
 console.log(likes(["Peter", "Jacob", "Alex"])); //"Max, John and Mark like this"
 console.log(likes(["Peter", "Jacob", "Alex", "Max"])); //"Alex, Jacob and 2 others like this"
+console.log(likes(["Peter", "Jacob", "Alex", "Max", "Alex", "Max"])); //"Alex, Jacob and 2 others like this"
