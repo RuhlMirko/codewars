a4f1013574065effef50c4fffcfbb3cfb7a95416
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-01 18:38:27 -0300

    Added new kata

---
+++ fundamentals/day36.5.js
@@ -0,0 +1,36 @@
+const outOfRange = [
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+function printerError(s) {
+  const matches = s.match(/[n-z]/gi);
+  if (!matches) return `0/${s.length}`;
+  return `${matches.length}/${s.length}`;
+}
+
+console.log(
+  printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
+); // 3/56
+console.log(
+  printerError("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
+); // 6/60
+console.log(
+  printerError(
+    "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyzuuuuu"
+  )
+); // 11/65
+console.log(
+  printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmm")
+); // 0/53
