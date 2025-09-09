54a88855b57af15dfb8db011d341e3d48a5b8c30
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-08 22:22:47 -0300

    Added new kata

--- fundamentals/day40.2.js
+++ fundamentals/day40.2.js
@@ -7,5 +7,10 @@ function oldGrow(x) {
   return sum;
 }
 
+// function grow(arr) {  return arr.reduce((acc, curr) => acc * curr, 1);}
+
+const grow = (arr) => arr.reduce((acc, curr) => acc * curr, 1);
+
 console.log(grow([1, 2, 3])); // 6
 console.log(grow([4, 1, 1, 1, 4])); // 16
+console.log(grow([2, 2, 2, 2, 2, 2])); // 64
