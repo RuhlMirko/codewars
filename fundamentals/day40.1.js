8ba64b5dd1622cc709eca33c6b792cc38eb4fc47
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-08 22:05:29 -0300

    Added new kata

---
+++ fundamentals/day40.1.js
@@ -0,0 +1,10 @@
+function rowSumOddNumbers(n) {
+  return n ** 3;
+}
+
+console.log(rowSumOddNumbers(1)); // 1
+console.log(rowSumOddNumbers(2)); // 8
+console.log(rowSumOddNumbers(3)); // 27
+console.log(rowSumOddNumbers(4)); // 64
+console.log(rowSumOddNumbers(5)); // 125
+console.log(rowSumOddNumbers(42)); // 74088
