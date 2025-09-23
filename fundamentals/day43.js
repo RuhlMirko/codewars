4cf0a51147ed86a1374ab1d0487bd7d93d753732
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-23 12:56:00 -0300

    Finished kata exercise

--- fundamentals/day43.js
+++ fundamentals/day43.js
@@ -1,17 +1,16 @@
 function getSum(a, b) {
-  console.log("Next sum:");
   let lowerNum = a < b ? a : b;
+  let higherNum = a > b ? a : b;
   let sum = lowerNum;
-  for (let i = lowerNum; i < b; i++) {
-    console.log(i);
+  for (let i = lowerNum + 1; i <= higherNum; i++) {
     sum += i;
   }
-
   return sum;
 }
 
 console.log(getSum(0, -1)); // -1
 console.log(getSum(0, 1)); // 1
 console.log(getSum(2, 2)); // 2
-console.log(getSum(2, 6)); // 2 + 4 + 6 = 12
+console.log(getSum(2, 6)); // 2 + 3 + 4 + 5 + 6 = 20
 console.log(getSum(-1, 2)); // 2
+console.log(getSum(5, -1)); // 14
