878b5f0d7a6df3fe81abc7ca778b7cfc0bbd3913
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-24 12:27:10 -0300

    Added improved version

--- fundamentals/day43.js
+++ fundamentals/day43.js
@@ -8,9 +8,23 @@ function getSum(a, b) {
   return sum;
 }
 
-console.log(getSum(0, -1)); // -1
-console.log(getSum(0, 1)); // 1
-console.log(getSum(2, 2)); // 2
-console.log(getSum(2, 6)); // 2 + 3 + 4 + 5 + 6 = 20
-console.log(getSum(-1, 2)); // 2
-console.log(getSum(5, -1)); // 14
+function newgetSum(a, b) {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  console.log(max - min + 1, min + max);
+  return ((max - min + 1) * (min + max)) / 2;
+}
+
+console.log(newgetSum(0, -1)); // -1
+console.log(newgetSum(0, 1)); // 1
+console.log(newgetSum(2, 2)); // 2
+console.log(newgetSum(2, 6)); // 20
+console.log(newgetSum(-1, 2)); // 2
+console.log(newgetSum(5, -1)); // 14
+
+// console.log(getSum(0, -1)); // -1
+// console.log(getSum(0, 1)); // 1
+// console.log(getSum(2, 2)); // 2
+// console.log(getSum(2, 6)); // 2 + 3 + 4 + 5 + 6 = 20
+// console.log(getSum(-1, 2)); // 2
+// console.log(getSum(5, -1)); // 14
