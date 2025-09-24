d29f7abe678bbe230bce5bb5928906b05ab15500
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-24 12:38:13 -0300

    Added one liner version

--- fundamentals/day44.js
+++ fundamentals/day44.js
@@ -11,3 +11,5 @@ console.log(removeEveryOther([1, 2, 3, 4, 5, 6])); // [1, 3, 5]
 console.log(removeEveryOther([[1, 2]])); // [[1, 2]]
 console.log(removeEveryOther([["Goodbye"], { Great: "Job" }])); // [['Goodbye']]
 console.log(removeEveryOther([1, 3, 4, 5, 6, 7, 8, 9, 10])); //[ 1, 3, 5, 7, 9 ]
+
+const newRemoveEveryOther = (arr) => arr.filter((_, i) => i % 2 === 0);
