98f01c76072fa70b1076d9cea43d634258ba373a
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-24 12:36:14 -0300

    Finished kata of the day

---
+++ fundamentals/day44.js
@@ -0,0 +1,13 @@
+function removeEveryOther(arr) {
+  let evenIndex = [];
+  for (let i in arr) {
+    if (i % 2 == 0) evenIndex.push(arr[i]);
+  }
+  return evenIndex;
+}
+
+console.log(removeEveryOther(["Hello", "Goodbye", "Hello Again"])); // ['Hello', 'Hello Again']
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6])); // [1, 3, 5]
+console.log(removeEveryOther([[1, 2]])); // [[1, 2]]
+console.log(removeEveryOther([["Goodbye"], { Great: "Job" }])); // [['Goodbye']]
+console.log(removeEveryOther([1, 3, 4, 5, 6, 7, 8, 9, 10])); //[ 1, 3, 5, 7, 9 ]
