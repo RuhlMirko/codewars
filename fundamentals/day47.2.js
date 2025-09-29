085ffd6ab1a1076f7d094fd3b12b1e5bf8bcf374
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-28 21:22:49 -0300

    Started max sequence kata

---
+++ fundamentals/day47.2.js
@@ -0,0 +1,21 @@
+//   0   1   2  3   4  5  6  7  8
+// [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+//  -2 -1 -4 \ 4 3 5 6 \ 1 5 == Largest sum is 6
+//            4 -1 2 1 \
+
+var maxSequence = function (arr) {
+  // Exceptions
+  let hasPositive = arr.filter((e) => e >= 0);
+  if (!arr.length || !hasPositive.length) return 0;
+  // truthy values
+  let sum = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    sum += arr[i];
+    console.log(sum);
+  }
+};
+
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6 as stated above
+console.log(maxSequence([])); // 0
+console.log(maxSequence([-2, -3, -3, -4, -6, -5])); // 0
+console.log(maxSequence([-6, 2, -4, 1, 3, -1, 5, -1])); // 8 [1,3,-1,5]
