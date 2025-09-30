f5637e3f068edbea51fb0a1ae35e29b4d6ff0213
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-30 00:47:42 -0300

    Added fourth kata of the day

---
+++ fundamentals/day49.3.js
@@ -0,0 +1,13 @@
+const quarterOf = (month) => Math.ceil(month / 3);
+
+console.log(quarterOf(3));
+console.log(quarterOf(4));
+console.log(quarterOf(8));
+console.log(quarterOf(11));
+
+/*
+1 2 3 
+4 5 6
+7 8 9
+10 11 12 
+ */
