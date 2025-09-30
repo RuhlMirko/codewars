5fed4d46f9711a76f9c3edbf1ada9f646033e824
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-30 00:26:48 -0300

    Added second exercise of the day

---
+++ fundamentals/day49.1.js
@@ -0,0 +1,8 @@
+function combat(health, damage) {
+  if (health - damage < 0) return 0;
+  return health - damage;
+}
+
+console.log(combat(100, 5)); //95
+console.log(combat(92, 8)); //84
+console.log(combat(20, 30)); //0
