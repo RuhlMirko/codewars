d3dff06496244952b38b7c1115b17455f85386b3
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-30 00:27:14 -0300

    Added one liner version

--- fundamentals/day49.1.js
+++ fundamentals/day49.1.js
@@ -3,6 +3,9 @@ function combat(health, damage) {
   return health - damage;
 }
 
+// One liner version
+const shortCombat = (health, damage) => Math.max(0, health - damage);
+
 console.log(combat(100, 5)); //95
 console.log(combat(92, 8)); //84
 console.log(combat(20, 30)); //0
