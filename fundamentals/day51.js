0a2ebd601bc426db7d245d385dbd0c98d73ed615
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-10-02 21:01:38 -0300

    Added kata of the day

---
+++ fundamentals/day51.js
@@ -0,0 +1,8 @@
+function noBoringZeros(n) {
+  return Number(String(n).replace(/0+$/, ""));
+}
+console.log(noBoringZeros(1450)); // 145
+console.log(noBoringZeros(960000)); // 96
+console.log(noBoringZeros(1050)); // 105
+console.log(noBoringZeros(-1050)); // -105
+console.log(noBoringZeros(0)); // 0
