6fa2267342070bb3a109fb1c819d5423eaec3525
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-12-08 12:25:12 -0300

    Site now calls and display a cat image

--- interviewPrep/pruebaTecnica1/src/App.jsx
+++ interviewPrep/pruebaTecnica1/src/App.jsx
@@ -1,10 +1,20 @@
+import React from 'react';
 export default function App() {
- 
+  const [catImageSrc, setCatImageSrc] = React.useState('')
+
+  React.useEffect(()=>{
+    fetch('https://catfact.ninja/fact?max_length=80')
+      .then(res=>res.json())      
+      .then(data=>`https://cataas.com/cat/says/${data.fact}?fit=crop&width=500&height=500`)
+        .then(url=>setCatImageSrc(url))
+  },[])
+
 
   return (
     <main>
       <h1>Hello Vite + React!</h1>
       <p>Start editing to see some magic happen</p>
+      <img src={catImageSrc} alt="" height='500px' />
     </main>
   );
 }
