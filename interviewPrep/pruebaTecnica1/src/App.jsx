8ff22f619a1362bb5ac5b290173c16520289edef
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-12-09 00:15:48 -0300

    Image now renders properly

--- interviewPrep/pruebaTecnica1/src/App.jsx
+++ interviewPrep/pruebaTecnica1/src/App.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 export default function App() {
   const [catImageSrc, setCatImageSrc] = React.useState('')
-
   
-    React.useEffect(()=>{
-      fetch('https://catfact.ninja/fact?max_length=80')
-        .then(res=>res.json())      
-        .then(data=>`https://cataas.com/cat/says/${data.fact}?fit=crop&width=500&height=500`)
-          .then(url=>setCatImageSrc(url))
-    },[])
-  
-
+  React.useEffect(()=>{
+    fetch('https://catfact.ninja/fact?max_length=80')
+      .then(res=>res.json())      
+      .then(data=>`https://cataas.com/cat/says/${data.fact}`)
+        .then(url=>setCatImageSrc(url))
+  }, []) 
 
   return (
     <main>
       <h1>Cat facts</h1>
       <p>Get a picture of a cute cat that has some facts about them.</p>
-      <img src={catImageSrc} alt="" height='500px' />
+      <img src={catImageSrc} alt="Image of a cat with a caption" height='500px' />
+      <br />
       <button>Make new image</button>
     </main>
   );
