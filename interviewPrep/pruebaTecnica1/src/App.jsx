a6bceda80a7a8b8831c2a1f72bad24188f31dd0e
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-12-09 00:13:37 -0300

    Added button to generate new images

--- interviewPrep/pruebaTecnica1/src/App.jsx
+++ interviewPrep/pruebaTecnica1/src/App.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 export default function App() {
   const [catImageSrc, setCatImageSrc] = React.useState('')
 
-  React.useEffect(()=>{
-    fetch('https://catfact.ninja/fact?max_length=80')
-      .then(res=>res.json())      
-      .then(data=>`https://cataas.com/cat/says/${data.fact}?fit=crop&width=500&height=500`)
-        .then(url=>setCatImageSrc(url))
-  },[])
+  
+    React.useEffect(()=>{
+      fetch('https://catfact.ninja/fact?max_length=80')
+        .then(res=>res.json())      
+        .then(data=>`https://cataas.com/cat/says/${data.fact}?fit=crop&width=500&height=500`)
+          .then(url=>setCatImageSrc(url))
+    },[])
+  
 
 
   return (
     <main>
-      <h1>Hello Vite + React!</h1>
-      <p>Start editing to see some magic happen</p>
+      <h1>Cat facts</h1>
+      <p>Get a picture of a cute cat that has some facts about them.</p>
       <img src={catImageSrc} alt="" height='500px' />
+      <button>Make new image</button>
     </main>
   );
 }
