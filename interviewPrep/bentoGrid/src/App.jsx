3d9bb0f2dae6c228af5b2a4d96064e3cd2160762
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-12-25 13:43:22 -0300

    Added call to action section
    Added a call to action section that has a linkedin and email button

--- interviewPrep/bentoGrid/src/App.jsx
+++ interviewPrep/bentoGrid/src/App.jsx
@@ -7,22 +7,36 @@ export default function App(){
 
       <h1>Mirko Ruhl</h1>
       <h3>Full Stack Developer</h3>
-      
-      <ul className='shortcut'>
+      {/*
+<ul className='shortcut'>
         <li><button>Github</button></li>
         <li><button>LinkedIn</button></li>
         <li><button>Email</button></li>
-      </ul>
+      </ul>*/
+      }
+      
 
       <nav>
         <ul>
-          <li><a href="">Home</a></li>
+          <li><a href="#home">Home</a></li>
           <li><a href="">About Me</a></li>
           <li><a href="">Projects</a></li>
           <li><a href="">Skills</a></li>
           <li><a href="">Contact</a></li>
         </ul>
       </nav>
+
+      <section className='cta'>
+        <div>
+          <h2>Let's Work Together</h2>
+          <p>Have a project in mind? I'm available for freelance work and collaborations.</p>
+        </div>
+        <div>
+          <button>LinkedIn</button>
+          <button>Email</button>
+        </div>
+        
+      </section>
 
       <div className='grid-container'>
         <div className='box' style={{ gridArea: 'box-1' }}>
