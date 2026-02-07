8722903e50f52b87d2134cf3a7cefa7665cfe90c
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-02-07 18:46:09 -0300

    Tested global keyword from node

--- freecodecamp/node-express-course-main/01-node-tutorial/02-globals.js
+++ freecodecamp/node-express-course-main/01-node-tutorial/02-globals.js
@@ -6,7 +6,9 @@
 // module     - info about current module (file)
 // process    - info about env where the program is being executed
 
-console.log(__dirname)
-setInterval(() => {
-  console.log('hello world')
-}, 1000)
+console.log(
+  `${__dirname}\n ${__filename} \n ${process}\n${require}\n${module}`,
+);
+setTimeout(() => {
+  console.log("hello world");
+}, 1000);
