83f26a367184958f969706f51bca051695e9ee0c
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-02-07 20:44:40 -0300

    Tested things in filesystem lecture

--- freecodecamp/node-express-course-main/01-node-tutorial/03-modules.js
+++ freecodecamp/node-express-course-main/01-node-tutorial/03-modules.js
@@ -3,8 +3,11 @@
 
 const names = require("./04-names.js");
 const sayHi = require("./05-utils.js");
+const data = require("./06-alternative-flavor.js");
 // import data from "./06-alternative-flavor";
 // import "./07-mind-grenade";
 sayHi("susan");
 sayHi(names.john);
 sayHi(names.peter);
+
+console.log(data);
