dbffd551999d97f68d01e2eb01eae48a1fcfac8e
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-02-07 20:01:00 -0300

    Updated imports to use es modules instead of commonJS

--- freecodecamp/node-express-course-main/01-node-tutorial/03-modules.js
+++ freecodecamp/node-express-course-main/01-node-tutorial/03-modules.js
@@ -1,9 +1,9 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only share minimum)
-const names = require('./04-names')
-const sayHi = require('./05-utils')
-const data = require('./06-alternative-flavor')
-require('./07-mind-grenade')
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
+import { john, peter } from "./04-names";
+import sayHi from "./05-utils";
+import data from "./06-alternative-flavor";
+import "./07-mind-grenade";
+sayHi("susan");
+sayHi(john);
+sayHi(peter);
