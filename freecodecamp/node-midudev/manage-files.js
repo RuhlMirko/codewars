4b9fac9b0f00caf689cd2bf48defcdfa37ca78d3
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-02-10 15:20:16 -0300

    Added writefile function

--- freecodecamp/node-midudev/manage-files.js
+++ freecodecamp/node-midudev/manage-files.js
@@ -1,4 +1,8 @@
-import { readFile } from "node:fs/promises";
+import { readFile, writeFile } from "node:fs/promises";
 
 const content = await readFile("./archivo.txt", "utf-8");
 console.log(content);
+
+const upperContent = content.toUpperCase();
+await writeFile("./archivo.txt", upperContent);
+console.log(`Archivo con nuevo contenido: \n${upperContent}`);
