99d3236d0d9a3551bcfddca3c8252799a43c9533
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-02-10 17:41:13 -0300

    Added permission to files manipulation

--- freecodecamp/node-midudev/manage-files.js
+++ freecodecamp/node-midudev/manage-files.js
@@ -1,15 +1,27 @@
 import { readFile, writeFile, mkdir } from "node:fs/promises";
 import { join, basename, extname } from "node:path";
 
-const content = await readFile("./archivo.txt", "utf-8");
-console.log(content);
+/* ------------ PERMISSION TO WORK ON FILES WITH TERMINAL ------------
+node --permission --allow-fs-read="./file.txt" app.js
+node --premission --allow-fs-read="./file2.txt" --allow-fs-write="./output/*" app.js
+*/
 
-const outputDir = join("output", "files", "docs");
-await mkdir(outputDir, { recursive: true });
+if (process.permission.has("fs.read", "archivo.txt")) {
+  const content = await readFile("./archivo.txt", "utf-8");
+  console.log(content);
+  const upperContent = content.toUpperCase();
+  const outputFilePath = join(outputDir, "archivo.txt");
+  console.log("Extension:", extname(outputFilePath));
+  console.log("Nombre archivo:", basename(outputFilePath));
+  await writeFile(outputFilePath, upperContent);
+  console.log(`Archivo con nuevo contenido: \n${upperContent}`);
+} else {
+  console.log("Access denied for file reading");
+}
 
-const upperContent = content.toUpperCase();
-const outputFilePath = join(outputDir, "archivo.txt");
-console.log("Extension:", extname(outputFilePath));
-console.log("Nombre archivo:", basename(outputFilePath));
-await writeFile(outputFilePath, upperContent);
-console.log(`Archivo con nuevo contenido: \n${upperContent}`);
+if (process.permission.has("fs.write", "output/files/docs")) {
+  const outputDir = join("output", "files", "docs");
+  await mkdir(outputDir, { recursive: true });
+} else {
+  console.log("Access denied for directory manipulation");
+}
