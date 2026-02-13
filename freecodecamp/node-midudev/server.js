bd8033c1bb7299e56173ee6f1907181e780e43ea
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-02-12 21:52:08 -0300

    Added json response

--- freecodecamp/node-midudev/server.js
+++ freecodecamp/node-midudev/server.js
@@ -1,12 +1,30 @@
+import { error } from "node:console";
 import { createServer } from "node:http";
 
 // const port = 3000;
+// Uses variable inside enviroment file to set the port
+process.loadEnvFile();
 const port = process.env.PORT ?? 3000;
 
+function sendJson(res, statusCode, data) {
+  res.statusCode = statusCode;
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.end(JSON.stringify(data));
+}
+
 const server = createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.end(`Hola desde node 🔥💯`);
+  res.statusCode = 200;
+  if (req.url === "/") {
+    return res.end(`Hola mundo, estas en la root`);
+  } else if (req.url === "/users") {
+    sendJson(res, 200, [
+      { id: 1, name: "midudev" },
+      { id: 2, name: "mouredev" },
+    ]);
+  }
+
+  sendJson(res, 404, { error: "Not Found" });
 });
 
 server.listen(port, () => {
