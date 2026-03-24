4fb4ef042afafd9f596057c2ecac3da63d32791b
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-03-24 12:29:30 -0300

    Added void and never return fn

--- scrimba-challenges/typescript/midu1.ts
+++ scrimba-challenges/typescript/midu1.ts
@@ -13,13 +13,24 @@ function saludar({name, age}:{name:string, age:string}){
 }
 
 
-
+// how to explicitly return a vcalue with type
 const sayHiFromFunction = (fn: (name: string) => void) => {
     fn('Miguel')
 }
-
 const sayHi = (name: string) => {
     console.log('Hola ${name}')
 }
-
 sayHiFromFunction (sayHi)
+
+// Arrow functions
+const sumar = (a:number, b:number): number => a + b
+const restar: (a:number, b:number) => number = (a,b) => a - b
+
+// never nunca hace return
+function throwErr(message:string): never{ throw new Error(message);}
+
+function logMessage(message: string): void{
+    console.log(message)
+    throw new Error(message)
+    // return existe preo no es de interes -> nunca devuelve nada importante
+}
