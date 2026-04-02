dca1bd8e1f794f0c8055549c1a5881d5164a4db5
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-04-02 01:51:13 -0300

    Added logger for all routes

--- freecodecamp/boilerplate-express/myApp.js
+++ freecodecamp/boilerplate-express/myApp.js
@@ -1,15 +1,23 @@
+require("dotenv").config();
 let express = require("express");
 let path = require("path");
 let app = express();
 
-console.log("Hello world");
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.get("/json", function (req, res) {
-  res.json({ message: "Hello json" });
+  const jsonResponse = {
+    message:
+      process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json",
+  };
+  res.json(jsonResponse);
 });
 
 const publicDir = __dirname + "/public";
