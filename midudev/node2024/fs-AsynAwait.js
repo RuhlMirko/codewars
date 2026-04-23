9422136b7ad5e3b654c64a319c4177135511c1e7
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-04-23 17:49:06 -0300

    Added async file reading examples

---
+++ midudev/node2024/fs-AsynAwait.js
@@ -0,0 +1,13 @@
+import fs from "node:fs/promises";
+
+console.log("Leyendo el primer archivo ... ");
+fs.readFile("./archivo.txt", "utf-8").then((text) =>
+  console.log("Primer texto:", text),
+);
+
+console.log("Using function");
+
+console.log("Leyendo el segundo archivo ...");
+fs.readFile("./archivo2.txt", "utf-8").then((text) =>
+  console.log("Segundo texto:", text),
+);
---
+++ midudev/node2024/fs-readFile.js
@@ -0,0 +1,21 @@
+import fs from "node:fs";
+
+// const { promisify } = require("node:util");  <- Convierte cualquier metodo en promesa si solo se importo la version sincronica
+// const readFilePromise = promisify(fs.readFile);
+
+// console.log("Leyendo el primer archivo... ");
+// fs.readFilePromise("./archivo.txt", "utf-8").then((text) => {
+//   console.log("primer texto:", text);
+// });
+
+console.log("Leyendo el primer archivo ... ");
+const text = fs.readFile("./archivo.txt", "utf-8", (err, text) =>
+  console.log(text),
+);
+
+console.log("Using function");
+
+console.log("Leyendo el segundo archivo ...");
+const secondText = fs.readFile("./archivo2.txt", "utf-8", (err, text) =>
+  console.log(text),
+);
