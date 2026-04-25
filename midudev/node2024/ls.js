dd07a73563ceaba976f972d381d97222b779cf2e
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-04-25 01:09:20 -0300

    Added promises cersion of file system reading ls

--- midudev/node2024/ls.js
+++ midudev/node2024/ls.js
@@ -7,3 +7,15 @@ fs.readdir(".", (err, files) => {
   }
   files.forEach((file) => console.log(file));
 });
+// import fs from "node:promises";
+
+// fs.readdir(".")
+//   .then((files) => {
+//     files.forEach((file) => console.log(file));
+//   })
+//   .catch((err) => {
+//     if (err) {
+//       console.log("Error reading directory: ", err);
+//       return;
+//     }
+//   });
