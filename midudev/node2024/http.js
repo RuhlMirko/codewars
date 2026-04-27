355c55809658cbd5090f7dfb0794ffb131313d52
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-04-27 08:54:16 -0300

    Added custom port function to http

--- midudev/node2024/http.js
+++ midudev/node2024/http.js
@@ -1,8 +1,17 @@
 const http = require('node:http')
+const { findAvailablePort } = require('./freeport.js')
+
+const deiredPort = process.env.PORT ?? 3000
 
 const server = http.createServer((req, res) => {
   console.log('Request')
   res.end('Hola mundo')
 })
 
-server.listen(0, () => { console.log(`Server listening on http://localhost:${server.address().port}`) })
+findAvailablePort(deiredPort).then(port => {
+  server.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`)
+  })
+})
+
+// server.listen(0, () => { console.log(`Server listening on http://localhost:${server.address().port}`) })
