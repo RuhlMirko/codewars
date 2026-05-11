5b981e08a0edcec875889c7cd27510993d3f5f50
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-05-11 11:53:50 -0300

    Added json data

--- midudev/express2024/2-routing.js
+++ midudev/express2024/2-routing.js
@@ -1,7 +1,19 @@
 const http = require('node:http')
+const dittoJson = require('./ditto.json')
 
 const processRequest = (request, response) => {
   const { url, method } = request
+
+  switch (url) {
+    case '/pokemon/ditto':
+      response.setHeader('Content-Type', 'application/json')
+      return response.end(JSON.stringify(dittoJson))      
+    case '/about':
+      return response.end('Welcome to the about page')
+    default:
+      response.statusCode = 404
+      return response.end('Page not found')
+  }
 }
 
 const server = http.createServer(processRequest)
